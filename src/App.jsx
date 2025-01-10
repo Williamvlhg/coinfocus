b9771cb35d954a7c4342f5bc18902a90ade1a04a
Williamvlhg <williamvlhg@gmail.com>
2025-01-10 13:22:09 +0100

    zqsd

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,21 @@
-import './App.css'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home'
-import Login from './pages/Connexion/Login';
-import Register from './pages/Connexion/Register';
-import Transaction from './pages/transactions/Transaction';
-function App() {
 
-  return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/register" element={<Register />} />
-        <Route path="/transaction" element={<Transaction />} />
-        <Route path="/" element={<Home />}/>
-      </Routes>
-    </Router>
-  )
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
 import TradingPage from './Pages/trading/TradingPage';
 import BlogPage from './Pages/blog/BlogPage';
 import CryptoDetailsPage from './Pages/crypto/CryptoDetailsPage';
 import Login from './pages/Connexion/Login';
 import Register from './pages/Connexion/Register';
 import Transaction from './Pages/transactions/Transaction';
-import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <Router>
       <div className="App">
-        <Navbar /> 
         <main>
           <Routes>
+            <Route path="/" element={<Home />}/>
             <Route path="/trading" element={<TradingPage />} />
             <Route path="/blog" element={<BlogPage />} />
             <Route path="/crypto/:cryptoId" element={<CryptoDetailsPage />} />
